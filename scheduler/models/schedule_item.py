feb81a16242f8e2b6066d27a32a83a39a47190a6
kawaic <kawaic@codebasis.net>
2013-10-16 16:12:33 -0400

    STAR-27 start conflict check

--- scheduler/models/schedule_item.py
+++ scheduler/models/schedule_item.py
@@ -11,6 +11,9 @@ class ScheduleItem(models.Model):
     day_of_week = models.CharField(max_length=3, choices=DAY_OF_WEEK_CHOICES)
     section = models.ForeignKey(Section)
 
+    def conflits_with(self, other_item):
+        return False
+
     def __unicode__(self):
         return "%s from %s to %s on %s" % (self.location, self.start_time,
                                            self.end_time, self.day_of_week)
--- scheduler/models/section.py
+++ scheduler/models/section.py
@@ -1,6 +1,7 @@
 from django.db import models
 from scheduler.models.course import Course
 from scheduler.models.semester import Semester
+from itertools import chain
 
 
 class Section(models.Model):
@@ -13,7 +14,32 @@ class Section(models.Model):
         return len(self.studentrecordentry_set.all()) < self.capacity
 
     def conflicts_with(self, section):
+
+        for my_item in self.all_schedule_items:
+            for test_item in section.all_schedule_items:
+                if my_item.conflicts_with(test_item):
+                    return True
         return False
+
+    @property
+    def all_schedule_items(self):
+        result_list = list(chain(self.lectures, self.labs, self.tutorials))
+        return result_list
+
+    @property
+    def lectures(self):
+        from scheduler.models import Lecture
+        return Lecture.objects.filter(section=self)
+
+    @property
+    def labs(self):
+        from scheduler.models import Lab
+        return Lab.objects.filter(section=self)
+
+    @property
+    def tutorials(self):
+        from scheduler.models import Tutorial
+        return Tutorial.objects.filter(section=self)
 
     def __unicode__(self):
         return str(self.course.name) + " " + str(self.name)
