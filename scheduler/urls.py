1b8e1bf21a41a56f2c2cb89113d3f80d9ea9d89c
kawaic <kawaic@codebasis.net>
2013-10-22 16:32:26 -0400

    STAR-29 store schedue in session

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
 
     url(r'^student', views.student, name='student'),
     url(r'^schedule', views.schedule, name='schedule'),
+    url(r'^add_course', views.add_course, name='add_course'),
     url(r'^register', views.register, name='register'),
     url(r'^drop', views.drop, name='drop'),
 
