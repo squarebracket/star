a9d767910dd36ed430de18a97977324a0751af95
kawaic <kawaic@codebasis.net>
2013-10-30 16:45:15 -0400

    STAR-29 adding unit tests for schedule

--- scheduler/tests/schedule_tests.py
+++ scheduler/tests/schedule_tests.py
@@ -14,4 +14,9 @@ class ScheduleTest(TestCase):
         soen_341 = [s for s in all_section if s.course.name == "SOEN 341" and s.semester_year.name == "Fall 2013"]
         self.assertEqual(1, len(soen_341))
         self.schedule.add_section(soen_341[0])
+
+        #Check we have something for a semester
         self.assertEqual(1, len(self.schedule.semester_schedules))
+
+        #Check we have an item in that semester
+        self.assertEqual(1, len(self.schedule.semester_schedules[0].schedule_items))
