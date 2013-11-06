49d115bb8654d8f3143b828f3d70e2a18b62cf52
Chuck Wilson <chuck.wilson@gmail.com>
2013-11-06 16:41:22 -0500

    Changing logic in ScheduleItem to handle the definition in Section

--- scheduler/models/schedule_item.py
+++ scheduler/models/schedule_item.py
@@ -5,29 +5,35 @@ from uni_info.models.section import Section
 
 
 class ScheduleItem(models.Model):
-    location = models.ForeignKey(Facility)
-    start_time = models.TimeField('start time')
-    end_time = models.TimeField('end time')
-    day_of_week = models.CharField(max_length=3, choices=DAY_OF_WEEK_CHOICES)
     section = models.ForeignKey(Section)
 
     def conflicts_with(self, other_item):
-        if self.day_of_week != other_item.day_of_week:
+        if type(other_item) is ScheduleItem:
+            other_item = other_item.section
+        elif type(other_item) is not Section:
+            #error
+            return
+        # check to see if the days overlap
+        for day in self.section.days:
+            if day in other_item.days:
+                break
+        else:
+            # if the for loop completes without breaking, the days don't overlap
             return False
-        if self.start_time <= other_item.start_time <= self.end_time:
+        if self.section.start_time <= other_item.start_time <= self.section.end_time:
             return True
-        if self.start_time <= other_item.end_time <= self.end_time:
+        if self.section.start_time <= other_item.end_time <= self.section.end_time:
             return True
-        if other_item.start_time <= self.start_time <= other_item.end_time:
+        if other_item.start_time <= self.section.start_time <= other_item.end_time:
             return True
-        if other_item.start_time <= self.end_time <= other_item.end_time:
+        if other_item.start_time <= self.section.end_time <= other_item.end_time:
             return True
 
         return False
 
     def __unicode__(self):
-        return "%s from %s to %s on %s for %s" % (self.location, self.start_time,
-                                                  self.end_time, self.day_of_week, str(self.section))
+        return "%s from %s to %s on %s for %s" % (self.section.location, self.section.start_time,
+                                                  self.section.end_time, self.section.days, self.section)
 
     class Meta:
         def __init__(self):
