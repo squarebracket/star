5860cf1d6823864186c8f4b4fadf5ecc9f90e937
kawaic <kawaic@codebasis.net>
2013-11-23 15:27:02 -0500

    adding unit tests

--- scheduler/tests/__init__.py
+++ scheduler/tests/__init__.py
@@ -3,3 +3,4 @@ from course_tests import *
 from semester_schedule_tests import *
 from section_tests import *
 from schedule_item_tests import *
+from schedule_generator_tests import *
--- scheduler/tests/schedule_generator_tests.py
+++ scheduler/tests/schedule_generator_tests.py
@@ -1,1 +1,48 @@
-__author__ = 'chacoheadhewo'
+from django.utils.unittest.case import TestCase
+from scheduler.models import ScheduleGenerator
+from uni_info.models import Semester, Course
+
+
+class ScheduleGeneratorTest(TestCase):
+    fixtures = ['/scheduler/fixtures/initial_data.json']
+
+    def setUp(self):
+        self.fall_2013_semester = [sem for sem in Semester.objects.all() if sem.name == 'Fall 2013'][0]
+
+    def test_should_generate_schedule_for_2_course(self):
+        soen341 = [s for s in Course.objects.all() if
+                   s.course_letters == 'SOEN' and
+                   s.course_numbers == '341'][0]
+
+        soen287 = [s for s in Course.objects.all() if
+                   s.course_letters == 'SOEN' and
+                   s.course_numbers == '287'][0]
+
+        course_list = [soen287, soen341]
+
+        generator = ScheduleGenerator(course_list, self.fall_2013_semester)
+        result = generator.generate_schedules()
+
+        self.assertIsNotNone(result)
+        self.assertEqual(4, len(result))
+
+    def test_should_not_generate_schedule_for_3_course_conflict(self):
+        soen341 = [s for s in Course.objects.all() if
+                   s.course_letters == 'SOEN' and
+                   s.course_numbers == '341'][0]
+
+        soen342 = [s for s in Course.objects.all() if
+                   s.course_letters == 'SOEN' and
+                   s.course_numbers == '342'][0]
+
+        soen287 = [s for s in Course.objects.all() if
+                   s.course_letters == 'SOEN' and
+                   s.course_numbers == '287'][0]
+
+        course_list = [soen287, soen341, soen342]
+
+        generator = ScheduleGenerator(course_list, self.fall_2013_semester)
+        result = generator.generate_schedules()
+
+        self.assertIsNotNone(result)
+        self.assertEqual(0, len(result))
