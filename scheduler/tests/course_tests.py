50266aa4ec6b5f777ec4561c84342cd208bd1024
kawaic <kawaic@codebasis.net>
2013-11-24 02:28:28 -0500

    adding unit tests

--- scheduler/tests/course_tests.py
+++ scheduler/tests/course_tests.py
@@ -3,38 +3,59 @@ from uni_info.models import Course, Semester, Department
 
 
 class CourseTest(TestCase):
+    """
+    Test class for Course configuration object
+    """
     fixtures = ['/scheduler/fixtures/initial_data.json']
 
     def setUp(self):
-        self.fall_2013_semester = [sem for sem in Semester.objects.all() if sem.name == 'Fall 2013'][0]
+        """
+        Set up common data needed in each unit test
+        """
+        self.fall_2013_semester = [sem for sem in Semester.objects.all()
+                                   if sem.name == 'Fall 2013'][0]
         self.department = Department.objects.all()[0]
 
     def test_should_find_by_id(self):
-        c = Course.objects.get(id=1)
-        self.assertIsNotNone(c)
-        self.assertEqual('ELEC', c.course_letters)
-        self.assertEqual('275', c.course_numbers)
-        self.assertEqual(1, c.openness)
+        """
+        Test that we can find by primary key, check attributes
+        """
+        course = Course.objects.get(id=1)
+        self.assertIsNotNone(course)
+        self.assertEqual('ELEC', course.course_letters)
+        self.assertEqual('275', course.course_numbers)
+        self.assertEqual(1, course.openness)
 
     def test_should_create_course(self):
-        c = Course()
-        c.department = self.department
-        c.openness = 0
-        c.save()
-        self.assertIsNotNone(c.id)
+        """
+        Test that we can create a course for a department, check existence
+        """
+        course = Course()
+        course.department = self.department
+        course.openness = 0
+        course.save()
+        self.assertIsNotNone(course.id)
+        check = Course.objects.get(course.id)
+        self.assertIsNotNone(check)
 
     def test_should_update_course(self):
-        c = Course.objects.all()[0]
-        course_id = c.id
-        c.openness = 1
-        c.save()
+        """
+        Test that we can update an attribute for a course
+        """
+        course = Course.objects.all()[0]
+        course_id = course.id
+        course.openness = 1
+        course.save()
         check = Course.objects.get(id=course_id)
         self.assertEqual(1, check.openness)
 
     def test_should_delete_course(self):
-        c = Course.objects.get(id=2)
-        self.assertIsNotNone(c)
-        c.delete()
+        """
+        Test that we can delete a course and that afterwards cannot be found
+        """
+        course = Course.objects.get(id=2)
+        self.assertIsNotNone(course)
+        course.delete()
         check = False
         try:
             Course.objects.get(id=2)
@@ -44,11 +65,19 @@ class CourseTest(TestCase):
         self.assertTrue(check)
 
     def test_should_filter_by_course_letters(self):
+        """
+        Test we can find by course letters, check each value after to ensure correctness
+        """
         soen_list = Course.objects.filter(course_letters='SOEN')
         self.assertIsNotNone(soen_list)
         self.assertEqual(18, len(soen_list))
+        for c in soen_list:
+            self.assertEqual("SOEN", c.course_letters)
 
     def test_should_filter_by_name_and_section(self):
+        """
+        Test we can find with regular expression and can group into semesters
+        """
         course_list = Course.search_by_regex(r'SOEN')
         semester_list = [4]
         sections_by_semester = []
--- scheduler/tests/section_tests.py
+++ scheduler/tests/section_tests.py
@@ -3,19 +3,68 @@ from uni_info.models import Semester, Section, Course
 
 
 class SectionTest(TestCase):
+    """
+    Test class for Section configuration objects
+    """
     fixtures = ['/scheduler/fixtures/initial_data.json']
 
     def setUp(self):
+        """
+        Set up common data needed in each unit test
+        """
         self.fall_2013_semester = [sem for sem in Semester.objects.all() if sem.name == 'Fall 2013'][0]
 
-    def test_should_create(self):
-        course = Course.objects.all()[0]
-        sec = Section()
-        sec.course = course
-        sec.sec_type = 1
-        sec.semester_year = self.fall_2013_semester
-        sec.save()
-
     def test_should_find_by_id(self):
-        sec = Section.objects.filter(id=1)
-        self.assertIsNotNone(sec)
+        section = Section.objects.get(id=1)
+        self.assertEqual(24, section.course_id)
+        self.assertEqual(4, section.semester_year_id)
+        self.assertEqual('IE', section.name)
+        self.assertEqual(6, section.sec_type)
+        self.assertIsNotNone(section)
+
+    def test_should_create_section(self):
+        """
+        Test that we can crete a section for a course in a semester
+        """
+        course = Course.objects.all()[0]
+        section = Section()
+        section.course = course
+        section.sec_type = 1
+        section.semester_year = self.fall_2013_semester
+        section.save()
+        self.assertIsNotNone(section.id)
+
+    def test_should_update_section(self):
+        """
+        Test that we can update the attribute of a section
+        """
+        section = Section.objects.get(id=2)
+        self.assertEqual(0, section.capacity)
+        section.capacity = 100
+        section.save()
+        check = Section.objects.get(id=2)
+        self.assertEqual(100, check.capacity)
+
+    def test_should_delete_section(self):
+        """
+        Test that we can delete a section
+        """
+        section = Section.objects.get(id=3)
+        self.assertIsNotNone(section)
+        section.delete()
+        check = False
+        try:
+            Section.objects.get(id=3)
+        except Section.DoesNotExist:
+            check = True
+        self.assertTrue(check)
+
+    def test_should_filter_by_type(self):
+        """
+        Test that we can find by type of section
+        """
+        section_list = Section.objects.filter(sec_type=6)
+        self.assertIsNotNone(section_list)
+
+        for s in section_list:
+            self.assertEqual(6, s.sec_type)
